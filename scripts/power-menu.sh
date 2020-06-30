7aa80f2244e8034ad539151997511795b6010ca2
q.marques58@gmail.com <q.marques58@gmail.com>
2020-06-30 13:24:29 +0200

    Add Picom config

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# to edit themes in place with live reload:
-
-# ag -l | entr -r $HOME/.config/rofi/on
-# use -normal-window flag on rofi if you want to not have to close rofi to get
-# back to your editor.
-# https://www.reddit.com/r/qtools/comments/amswu9/autoloading_rofi_for_quick_themeing_experience/
-
 options="  lock
     logout
     sleep
@@ -15,21 +8,44 @@ options="  lock
     shutdown"
 themes_dir=$HOME/dotfiles/rofi
 theme=${1:-$themes_dir/onoff.rasi}
-selection=$(echo -e "${options}" | rofi -dmenu -config $theme -columns 6 -lines 1 -width 1500)
+#selection=$(echo -e "${options}" | rofi -dmenu -config $theme -columns 6 -lines 1 -width 1500)
 
-echo "This is your selection: $selection"
+chosen=$(echo -e " lock\n logout\n sleep\n hibernate\n reboot\n shutdown" | rofi -dmenu -config $theme -columns 6 -lines 1 -width 1500)
 
-case "${selection}" in
-    "      lock")
-        i3lock-fancy-multimonitor -n -p;;
-    "       logout")
-        i3-msg exit;;
-    "        sleep")
-        i3lock-fancy-multimonitor -n -p && systemctl suspend;;
-    "   hibernate")
-        i3lock-fancy-multimonitor -n -p && systemctl hibernate;;
-    "      reboot")
-        reboot;;
-    "      shutdown")
-        poweroff -i;;
-esac
+echo "This is your selection: $chosen"
+
+if [[ $chosen = " lock" ]]; then
+	/usr/bin/i3lock-fancy-multimonitor -n -p
+elif [[ $chosen = " logout" ]]; then
+	/usr/bin/i3-msg exit
+  echo "exit i3"
+elif [[ $chosen = " sleep" ]]; then
+  /usr/bin/i3lock-fancy-multimonitor -n -p && /usr/bin/systemctl suspend
+elif [[ $chosen = " hibernate" ]]; then
+  /usr/bin/i3lock-fancy-multimonitor -n -p && /usr/bin/systemctl hibernate
+elif [[ $chosen = " reboot" ]]; then
+  /usr/bin/reboot
+elif [[ $chosen = " shutdown" ]]; then
+  /usr/bin/poweroff -i
+fi
+
+#case "${selection}" in
+#    	"      lock")
+#        	i3lock-fancy-multimonitor -n -p
+#		;;
+#    	"       logout")
+#        	i3-msg exit
+#		;;
+#    	"        sleep")
+#        	i3lock-fancy-multimonitor -n -p && systemctl suspend
+#		;;
+#    	"   hibernate")
+#        	i3lock-fancy-multimonitor -n -p && systemctl hibernate
+#		;;
+#    	"      reboot")
+#        	reboot
+#		;;
+#    	"      shutdown")
+#        	poweroff -i
+#		;;
+#esac
